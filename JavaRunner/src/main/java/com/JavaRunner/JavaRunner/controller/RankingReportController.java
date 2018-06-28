475a763853d7f10c5a6a2718683a0111cbe87bdc
gfc95 <giovanifcunha@gmail.com>
2018-06-28 00:59:24 -0300

    relatorios
    relatorios

---
+++ JavaRunner/src/main/java/com/JavaRunner/JavaRunner/controller/RankingReportController.java
@@ -0,0 +1,60 @@
+package com.JavaRunner.JavaRunner.controller;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/admin/rankingReport")
+
+public class RankingReportController extends HttpServlet {
+
+    @GetMapping(value = "ranking")
+    @ResponseBody
+    public String RankingReport(HttpServletResponse response, HttpServletRequest request) throws JRException, IOException, ClassNotFoundException, SQLException {
+        Connection conexao = null;
+        Class.forName("com.mysql.jdbc.Driver");
+        conexao = DriverManager.getConnection("jdbc:mysql://localhost/tonhao_runner", "root", "");
+
+        Map<String,Object> params = new HashMap<>();
+
+        InputStream jasperStream = this.getClass().getResourceAsStream("/reports/rankingReport.jasper");
+        //  String relatorio = getServletContext().getRealPath("/relatorios") + "/Relatorio_Administradores.jasper";
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperStream, params, conexao);
+        // JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+
+
+        //  response.setContentType("application/x-pdf");
+        // response.setHeader("Content-disposition", "inline; filename=report_atleta.pdf");
+        byte[] relat = JasperExportManager.exportReportToPdf(jasperPrint); // exportar para pdf
+        response.setHeader("Content-Disposition", "attachment;filename=Relatorio de Rankings.pdf");
+        response.setContentType("application/x-pdf");
+        ServletOutputStream out = response.getOutputStream();
+
+        response.getOutputStream().write(relat);
+
+        final OutputStream outStream = response.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+
+        return "redirect:admin/index";
+    }
+
+}
